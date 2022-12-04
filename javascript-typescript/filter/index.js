b15ff5faccc8cf9e57463b1da04fc2203a02c07c
Gustavo <gustavoaugustocar@hotmail.com>
2022-12-04 18:06:51 -0300

    filter

---
+++ javascript-typescript/filter/index.js
@@ -0,0 +1,5 @@
+const arr = [{ message: 'teste' }, { message: 'teste' }, { message: 'teste' }, { message: 'teste' }, { message: 'oi' }];
+
+const filteredArr = arr.filter(item => item.message === 'teste'); // retorna os items do array onde a condicao for TRUE;
+
+console.log(filteredArr);
