d5db3c36e394b5bd2f61e22b629ffb11e854bec2
Gustavo <gustavoaugustocar@hotmail.com>
2022-12-11 14:28:38 -0300

    api de middleware funciona

---
+++ nestjs/middleware/src/app.module.ts
@@ -0,0 +1,16 @@
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { AuthMiddleware } from './middlewares/Auth/auth.middleware';
+
+@Module({
+  imports: [],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(AuthMiddleware).forRoutes('');
+  }
+}
---
+++ nestjs/middleware/src/middlewares/Auth/auth.middleware.ts
@@ -0,0 +1,13 @@
+import { Injectable, NestMiddleware } from '@nestjs/common';
+import { Request, Response, NextFunction } from 'express';
+
+@Injectable()
+export class AuthMiddleware implements NestMiddleware {
+  use(req: Request, res: Response, next: NextFunction) {
+    console.log('Caiu no middleware');
+
+    console.log({ req, res });
+
+    next();
+  }
+}
