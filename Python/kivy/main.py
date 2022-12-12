b8b6c7c7ae0b5ee3b327c1c067e855dcda4774a6
Gustavo <gustavoaugustocar@hotmail.com>
2022-12-12 20:01:27 -0300

    abre janela

---
+++ Python/kivy/main.py
@@ -0,0 +1,18 @@
+from kivymd.app import MDApp
+from kivy.lang import Builder
+
+kv = '''    
+MDScreen:
+    MDSegmentedControl:
+        pos_hint: {'center_x': .5, 'center_y': .5}
+
+        MDSegmentedControlItem:
+            text: "Python"
+
+'''
+
+class Window(MDApp):
+    def build(self):
+        return Builder.load_string(kv)
+
+Window().run()
