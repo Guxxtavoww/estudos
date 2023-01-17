7ed867428d33014ea2443f7457bb93d7a2585a6c
Gustavo <gustavoaugustocar@hotmail.com>
2023-01-17 13:44:52 -0300

    adding clicked outside

--- react/ref/src/App.tsx
+++ react/ref/src/App.tsx
@@ -1,8 +1,13 @@
 import React, { useCallback, useEffect, useRef } from "react";
 
+import { useClickOutside } from "./hooks/useClickOutside";
+
 const App: React.FC = () => {
   const renderCountRef = useRef<number>(0);
+  const elRef = useRef<HTMLElement>(null);
   const loginInputRef = useRef<HTMLInputElement>(null);
+
+  const { isClickedOutside } = useClickOutside(elRef);
 
   const handleFormSubmit = useCallback(
     (e: React.FormEvent<HTMLFormElement>) => {
@@ -17,10 +22,19 @@ const App: React.FC = () => {
     renderCountRef.current = renderCountRef.current++;
   });
 
+  useEffect(() => {
+    console.log(isClickedOutside);
+    alert(
+      isClickedOutside
+        ? "Voce clicou fora do texto de render"
+        : "Clicou dentro"
+    );
+  }, [isClickedOutside]);
+
   return (
     <form className="app_wrapper" onSubmit={handleFormSubmit}>
       <input className="s" ref={loginInputRef} />
-      <span>Renderizou {renderCountRef.current} vezes</span>
+      <span ref={elRef}>Renderizou {renderCountRef.current} vezes</span>
     </form>
   );
 };
---
+++ react/ref/src/hooks/useClickOutside.ts
@@ -0,0 +1,21 @@
+import { useState, useEffect, useCallback } from "react";
+
+export function useClickOutside(currentRef: React.RefObject<HTMLElement>) {
+  const [isClickedOutside, setIsClickedOutside] = useState(false);
+
+  const handleDocumentClick = useCallback((e: MouseEvent) => {
+    if (!currentRef.current?.contains(e.target as Node)) {
+      setIsClickedOutside(true);
+      return;
+    }
+    setIsClickedOutside(false);
+  }, []);
+
+  useEffect(() => {
+    document.addEventListener("mousedown", handleDocumentClick);
+
+    return () => document.removeEventListener("mousedown", handleDocumentClick);
+  }, []);
+
+  return { isClickedOutside };
+}
