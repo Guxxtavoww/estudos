5eb9622f0b54e6f6e7cbb2300fd0c6b9f6b0f017
Gustavo Augusto <81970233+Guxxtavoww@users.noreply.github.com>
2023-01-18 15:44:11 -0300

    update

--- react/ref/src/hooks/useClickOutside.ts
+++ react/ref/src/hooks/useClickOutside.ts
@@ -1,21 +1,24 @@
-import { useState, useEffect, useCallback } from "react";
+import { useState, useEffect, useCallback } from 'react';
 
-export function useClickOutside(currentRef: React.RefObject<HTMLElement>): boolean {
+export function useClickOutside(
+  currentRef: React.RefObject<HTMLElement>
+): boolean {
   const [isClickedOutside, setIsClickedOutside] = useState<boolean>(false);
 
-  const handleDocumentClick = useCallback((e: MouseEvent) => {
-    if (!currentRef.current?.contains(e.target as Node)) {
-      setIsClickedOutside(true);
-      return;
-    }
-    setIsClickedOutside(false);
-  }, [currentRef]);
+  const handleDocumentClick = useCallback(
+    (e: MouseEvent) => {
+      return setIsClickedOutside(
+        !!currentRef.current?.contains(e.target as Node)
+      );
+    },
+    [currentRef]
+  );
 
   useEffect(() => {
-    document.addEventListener("mousedown", handleDocumentClick);
+    document.addEventListener('mousedown', handleDocumentClick);
 
-    return () => document.removeEventListener("mousedown", handleDocumentClick);
-  }, []);
+    return () => document.removeEventListener('mousedown', handleDocumentClick);
+  }, [handleDocumentClick]);
 
   return isClickedOutside;
 }
