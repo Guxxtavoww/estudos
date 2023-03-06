0c2978185d1751bd9a8281ca20bc975d1182b57f
Gustavo <galmeida@pdasolucoes.com.br>
2023-03-06 13:45:34 -0300

    adding stuff

---
+++ Python/decorators/main.py
@@ -0,0 +1,15 @@
+import time
+
+def countFunctionRunTime(func):
+    def wrapper():
+        t1 = time.time()
+        func()
+        t2 = time.time() - t1
+        print(f'{func.__name__} rodou por {t2} segundos antes de finalizar')
+    return wrapper
+
+@countFunctionRunTime
+def test():
+    print('Teste')
+
+test()
